db60a203fa10cc64b86ad151a13325caa8f8599c
mitul bhatiya <mitulbhatiya3360@gmail.com>
2022-08-27 13:35:47 +0530

    Major Project

---
+++ lib/pages/learn_flutter.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_builder/pages/widget_structure_screen.dart';
+
+import 'home_screen.dart';
+
+
+class Learn_Flutter extends StatefulWidget {
+  const Learn_Flutter({Key key}) : super(key: key);
+
+  @override
+  _Learn_FlutterState createState() => _Learn_FlutterState();
+}
+
+class _Learn_FlutterState extends State<Learn_Flutter> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Learn Flutter"),
+        actions: [IconButton(icon:Icon(Icons.clear),onPressed: _triggerExitPageDialog)],
+      ),
+    );
+  }
+  void _triggerExitPageDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Return to home screen?"),
+          content: Text("This action cannot be undone"),
+          actions: <Widget>[
+            FlatButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Cancel")),
+            FlatButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => HomeScreen(),
+                  ),
+                );
+              },
+              child: Text("OK"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
